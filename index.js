f083792e921d464246c2bdb99945c8212466f4b1
KatheGalviz <laura.galviz@finanzauto.com.co>
2024-04-04 21:12:54 -0500

    comentado delete

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { connectDB, client } = require('./config');
-
+const { connectDB, client, ObjectId } = require('./config'); 
 class PiaApi {
     constructor() {
         this.app = express();
@@ -13,7 +12,7 @@ class PiaApi {
             await connectDB();
         } catch (error) {
             console.error('Error al conectar a la base de datos:', error);
-            process.exit(1); 
+            process.exit(1);
         }
     }
 
@@ -21,6 +20,9 @@ class PiaApi {
         this.app.use(express.json());
         this.app.post('/users', (req, res) => this.createUser(req, res));
         this.app.get('/users', (req, res) => this.getUsers(req, res));
+        this.app.get('/users/:id', (req, res) => this.getUserById(req, res));
+        this.app.delete('/users/:id', (req, res) => this.deleteUserById(req, res));
+        this.app.put('/users/:id', (req, res) => this.updateUserById(req, res));
     }
 
     createUser = async (req, res) => {
@@ -38,7 +40,7 @@ class PiaApi {
                     apellido,
                     cedula
                 };
-                res.json({ message: 'Usuario creado exitosamente'});
+                res.json({ message: 'Usuario creado exitosamente', user: newUser });
             } else {
                 console.error('No se pudo crear el usuario');
                 throw new Error('No se pudo crear el usuario');
@@ -48,7 +50,7 @@ class PiaApi {
             res.status(500).json({ message: 'Error al crear usuario' });
         }
     };
-    
+
     getUsers = async (req, res) => {
         try {
             const db = client.db();
@@ -60,6 +62,61 @@ class PiaApi {
             res.status(500).json({ message: 'Error al obtener usuarios' });
         }
     };
+
+/*     getUserById = async (req, res) => {
+        const userId = req.params.id;
+        try {
+            const db = client.db();
+            const collection = db.collection('users');
+            const user = await collection.findOne({ _id: ObjectId(userId) });
+            if (!user) {
+                res.status(404).json({ message: 'Usuario no encontrado' });
+            } else {
+                res.json({ user });
+            }
+        } catch (error) {
+            console.error('Error al obtener usuario por ID:', error);
+            res.status(500).json({ message: 'Error al obtener usuario por ID' });
+        }
+    };
+
+    deleteUserById = async (req, res) => {
+        const userId = req.params.id;
+        try {
+            const db = client.db();
+            const collection = db.collection('users');
+            const result = await collection.deleteOne({ _id: ObjectId(userId) });
+            if (result.deletedCount === 1) {
+                res.json({ message: 'Usuario eliminado exitosamente' });
+            } else {
+                res.status(404).json({ message: 'Usuario no encontrado' });
+            }
+        } catch (error) {
+            console.error('Error al eliminar usuario por ID:', error);
+            res.status(500).json({ message: 'Error al eliminar usuario por ID' });
+        }
+    };
+
+    updateUserById = async (req, res) => {
+        const userId = req.params.id;
+        const { nombre, apellido, cedula, contrasena } = req.body;
+        try {
+            const db = client.db();
+            const collection = db.collection('users');
+            const result = await collection.updateOne(
+                { _id: ObjectId(userId) },
+                { $set: { nombre, apellido, cedula, contrasena } }
+            );
+            if (result.modifiedCount === 1) {
+                res.json({ message: 'Usuario actualizado exitosamente' });
+            } else {
+                res.status(404).json({ message: 'Usuario no encontrado' });
+            }
+        } catch (error) {
+            console.error('Error al actualizar usuario por ID:', error);
+            res.status(500).json({ message: 'Error al actualizar usuario por ID' });
+        }
+    }; */
 
     startServer() {
         this.setupDB().then(() => {
